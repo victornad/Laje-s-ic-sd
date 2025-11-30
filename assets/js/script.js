a1ea345e9afab0da1098099bd2caf00f3b299209
Luis <luiscbggomes@gmail.com>
2025-11-30 12:56:14 -0300

    Tive que tirar o zip, tava muito grande

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 // Script simples para rolagem suave nos links do menu
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
+  anchor.addEventListener('click', function (e) {
     e.preventDefault();
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
     });
   });
 });
+
+
